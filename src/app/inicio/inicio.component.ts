07720f35a0b676e1120805be5cf3a4c63fb54058
Dimas-Miskevish <dimas_miskevish@hotmail.com>
2023-08-27 09:31:57 -0300

    Edicion de nabvar

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -15,26 +15,21 @@ export class InicioComponent {
       image: '../../assets/galeria1.jpg',
       thumbImage: '../../assets/galeria1.jpg',
       alt: 'Image 1',
-      title: 'Image 1'
     }, {
       image: '../../assets/galeria2.jpg',
       thumbImage: '../../assets/galeria2.jpg',
-      title: 'Image 2',
       alt: 'Image 2'
     }, {
       image: '../../assets/galeria3.jpg',
       thumbImage: '../../assets/galeria3.jpg',
-      title: 'Image 3',
       alt: 'Image 3'
     }, {
       image: '../../assets/galeria4.jpg',
       thumbImage: '../../assets/galeria4.jpg',
-      title: 'Image 4',
       alt: 'Image 4'
     }, {
       image: '../../assets/galeria5.jpg',
       thumbImage: '../../assets/galeria5.jpg',
-      title: 'Image 5',
       alt: 'Image 5'
     }
 ];
